03fc32e00ffaf523304cb46c5f86a886f2d7ffa0
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2026-01-07 21:36:51 -0500

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -64,7 +64,7 @@ export const headerData = {
       text: 'Landing',
       links: [
         {
-          text: 'Lead Generation',
+          text: 'Lend & Borrow',
           href: getPermalink('/landing/lead-generation'),
         },
         {
